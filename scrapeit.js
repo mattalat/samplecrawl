0a25aea55b2c243314eb52e1e4fa1236062eb786
Matt Lawrence <smallpigbigbear@yahoo.com>
2016-04-28 18:08:54 -0400

    Basic init

---
+++ scrapeit.js
@@ -0,0 +1,45 @@
+var Crawler = require('Crawler'),
+url = require('url'), 
+$ = require('cheerio');
+
+var pagesHit = 0,
+DATA = []; // can store data in local buffer, or Crawler cash, or handoff, etc
+
+var c = new Crawler({
+	maxConnections : 1,
+    callback : function (error, result, $) {
+	   	if(!result.body) return; // disregard odd results
+
+	   	console.log(result.url);
+
+    	var html; // the body of the page
+
+    	// Try to load
+	   	try{
+	   		html = $.load(result.body);
+	   	} catch (e) { 
+	   		console.error(e); 
+	   		return; // exit this page without spreading
+	   	}
+	   	pagesHit++;
+
+	   	console.log('Crawled ' + pagesHit + ': ' + result.request.uri.href);
+
+        // Keep crawling
+        html('a').each(function(index, a){
+        	try {
+	        	var queueUrl = $(a).attr('href');
+	        	links.push(queueUrl);
+	        	c.queue(queueUrl);
+	        } catch (e) {
+	        	console.error(e);
+	        }
+        })
+    }
+});
+
+
+
+// Fire it up
+var SITE_TO_VISIT = '';
+c.queue(SITE_TO_VISIT);
